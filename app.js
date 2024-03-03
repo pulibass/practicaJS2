dab597fd85cb6d4fade204b8b74fe6a5c87f2493
pulibass <edu24689@gmail.com>
2024-03-03 18:33:34 -0300

    actualizando

--- app.js
+++ app.js
@@ -170,6 +170,46 @@ numeroDivisible() */
 numeroDivisible() */
 
 
+/* //Escribir una función llamada contrasenaValida que reciba un string y retorne true si el string es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9".
+//De lo contrario debe retornar false.
+// escribe tu respuesta acá
+
+function contrasenaValida(password) {
+    if (password === "2Fj(jjbFsuj" || password === "eoZiugBf&g9") {
+        return true
+    } else {
+        return false
+    }
+}
+
+// código de prueba
+console.log(contrasenaValida("2Fj(jjbFsuj")) // true
+console.log(contrasenaValida("eoZiugBf&g9")) // true
+console.log(contrasenaValida("hola")) // false
+console.log(contrasenaValida("")) // false */
+
+
+//Escribir una función llamada calcularImpuestos que reciba dos argumentos numéricos: edad e ingresos. 
+//Si edad es igual o mayor a 18 y los ingresos son iguales o mayores a 1000 debe retornar ingresos * 40%. De lo contrario retornar 0.
+
+// escribe tu respuesta acá
+
+
+function calcularImpuestos(edad, ingresos) {
+    let ingresoCalculo = 40;
+    if (edad >= 18 && ingresos >= 1000) {
+        return ingresos = (ingresos * ingresoCalculo) / 100;
+    } else {
+        return ingreso = 0;
+    }
+}
+
+// código de prueba
+console.log(calcularImpuestos(18, 1000)) // 400
+console.log(calcularImpuestos(40, 10000)) // 4000
+console.log(calcularImpuestos(17, 5000)) // 0
+console.log(calcularImpuestos(30, 500)) // 0
+
 
 
 
