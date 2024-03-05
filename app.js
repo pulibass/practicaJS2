c8782e47150cc1de45707bfc02aa6045dc7cb951
pulibass <edu24689@gmail.com>
2024-03-04 23:08:51 -0300

    actualizando

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ numeroDivisible() */
 }
 numeroDivisible() */
 
-
+//1. Contraseña válida
 /* //Escribir una función llamada contrasenaValida que reciba un string y retorne true si el string es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9".
 //De lo contrario debe retornar false.
 // escribe tu respuesta acá
@@ -188,14 +188,14 @@ console.log(contrasenaValida("eoZiugBf&g9")) // true
 console.log(contrasenaValida("hola")) // false
 console.log(contrasenaValida("")) // false */
 
-
-//Escribir una función llamada calcularImpuestos que reciba dos argumentos numéricos: edad e ingresos. 
+//2. Calcular impuestos
+//Escribir una función llamada calcularImpuestos que reciba dos argumentos numéricos: edad e ingresos.
 //Si edad es igual o mayor a 18 y los ingresos son iguales o mayores a 1000 debe retornar ingresos * 40%. De lo contrario retornar 0.
 
 // escribe tu respuesta acá
 
 
-function calcularImpuestos(edad, ingresos) {
+/* function calcularImpuestos(edad, ingresos) {
     let ingresoCalculo = 40;
     if (edad >= 18 && ingresos >= 1000) {
         return ingresos = (ingresos * ingresoCalculo) / 100;
@@ -208,9 +208,63 @@ function calcularImpuestos(edad, ingresos) {
 console.log(calcularImpuestos(18, 1000)) // 400
 console.log(calcularImpuestos(40, 10000)) // 4000
 console.log(calcularImpuestos(17, 5000)) // 0
-console.log(calcularImpuestos(30, 500)) // 0
+console.log(calcularImpuestos(30, 500)) // 0 */
 
 
+
+//3. IMC (ïndice de masa corporal)
+//El índice de masa corporal(IMC), o BMI por sus siglas en inglés, es un valor que determina la cantidad de grasa de una persona.
+
+//El BMI se calcula con la siguiente formula: peso / altura ^ 2
+
+//Escribir una función llamada bmi que reciba dos argumentos: peso y altura, y retorne un string con las siguientes posibilidades:
+
+//"Bajo de peso" si el BMI < 18.5
+//"Normal" si está entre 18.5 y 24.9
+//"Sobrepeso" si está entre 25 y 29.9
+//"Obeso" si es igual o mayor a 30
+
+/* function bmi(peso, altura) {
+
+    let calculo = peso / (altura ** 2)
+
+    if (calculo < 18.5) {
+        console.log("Bajo de peso")
+    } else if (calculo >18.5 && calculo < 24.9) {
+        console.log("Normal")
+    } else if (calculo > 25 && calculo < 29.9) {
+        console.log("Sobrepeso")
+    } else if (calculo > 30) {
+        console.log("Obeso")
+    }
+    return calculo
+}
+
+// escribe la función bmi acá
+
+// código de prueba
+console.log(bmi(65, 1.8)) // "Normal"
+console.log(bmi(72, 1.6)) // "Sobrepeso"
+console.log(bmi(52, 1.75)) //  "Bajo de peso"
+console.log(bmi(135, 1.7)) // "Obeso" */
+
+
+//4. Imprimir un arreglo
+//Escribir una función llamada imprimirArreglo que reciba un arreglo e imprima cada elemento en una línea a parte:
+
+// escribe tu respuesta acá
+
+/* function imprimirArreglo(arreglo) {
+    for (let i = 0; i < arreglo.length; i++) {
+        console.log(arreglo[i])
+    }
+}
+// código de prueba
+console.log(imprimirArreglo([1, "Hola", 2, "Mundo"]))
+// 1
+// Hola
+// 2
+// Mundo */
 
 
 
