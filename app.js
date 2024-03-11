4ccc0b75b806825bfd79c6a5dffd17f00486fbae
pulibass <edu24689@gmail.com>
2024-03-11 14:20:04 -0300

    actualizando

--- app.js
+++ app.js
@@ -568,3 +568,38 @@ multiploWhile(12) */
     return console.log("la suma total de los pares es " + total)
 }
 paresWhile() */
+
+
+/* 13. Remover ceros
+Escribir una función llamada removerCeros que reciba un arreglo de números y retorne un nuevo arreglo excluyendo los ceros(0). */
+
+// escribe tu respuesta acá
+/* function removerCeros(ceros) {
+    const arregloNew = ceros.filter((arreglo) => arreglo !== 0)
+    return arregloNew
+}
+// código de prueba
+console.log(removerCeros([0, 1, 0, 2, 0, 3])) // [1, 2, 3]
+console.log(removerCeros([9, 3, 6, 4])) // [9, 3, 6, 4]
+console.log(removerCeros([0, 0, 0])) // []
+ */
+
+/* 4. Sumar arreglo entre el rango
+Escribir una función llamada sumarArreglo que reciba tres argumentos: un arreglo de números, la posición inicial y la posición final.
+La función debe retornar la suma de todos los números dentro del rango(la posición inicial y la posición final, incluyéndolas).
+Nota: puedes asumir que la posición inicial va a ser menor o igual a la posición final, y que están dentro de los límites del arreglo. */
+
+// escribe tu respuesta acá
+
+/* function sumarArreglo(arreglo, i, f) {
+    let suma = 0;
+    for(let j = i; i <= f; i++){
+        suma += arreglo[i]
+    }
+    return suma
+}
+
+// código de prueba
+console.log(sumarArreglo([1, 2, 3], 1, 2)); // 5
+console.log(sumarArreglo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3, 6)); // 22
+console.log(sumarArreglo([7, 8, 9], 0, 0)); // 7 */
